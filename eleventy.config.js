fbc5a4174ec952e40885c940642a6d41962c7159
Wayne Graham <wgraham@clir.org>
2024-09-21 07:56:58 -0400

    Updates for GitHub and Vite

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,86 +1,87 @@
-const path = require('path')
-const { resolve } = require('path')
+const path = require("path");
+const { resolve } = require("path");
 
 const { DateTime } = require("luxon");
 const markdownItAnchor = require("markdown-it-anchor");
-const markdownItAttrs = require('markdown-it-attrs');
+const markdownItAttrs = require("markdown-it-attrs");
 
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginBundle = require("@11ty/eleventy-plugin-bundle");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 const { EleventyHtmlBasePlugin } = require("@11ty/eleventy");
-const EleventyVitePlugin = require('@11ty/eleventy-plugin-vite')
+const EleventyVitePlugin = require("@11ty/eleventy-plugin-vite");
 
+const pluginImages = require("./eleventy.images.js");
 
-const pluginImages = require('./eleventy.images.js')
-
-module.exports = function(eleventyConfig) {
-
-	eleventyConfig.addPassthroughCopy('src/assets');
+module.exports = function (eleventyConfig) {
+	eleventyConfig.addPassthroughCopy("src/assets");
+	eleventyConfig.addPassthroughCopy("public");
 
 	// Watch content images for the image pipeline.
-	eleventyConfig.addWatchTarget("src/**/*.{svg,webp,png,jpeg}");
+	eleventyConfig.addWatchTarget("src/**/*.{svg,webp,png,jpeg,pdf}");
 
 	// Official Plugins
 	eleventyConfig.addPlugin(pluginNavigation);
-	eleventyConfig.addPlugin(pluginImages)
+	eleventyConfig.addPlugin(pluginImages);
 
 	eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
 	eleventyConfig.addPlugin(pluginBundle);
 
 	eleventyConfig.addPlugin(EleventyVitePlugin, {
-		tempFolderName: '.11ty-vite', // Default name of the temp folder
-	
-		root: path.resolve(__dirname, 'src'),
-	
+		tempFolderName: ".11ty-vite", // Default name of the temp folder
+
+		root: path.resolve(__dirname, "src"),
+
 		// Options passed to the Eleventy Dev Server
 		// e.g. domdiff, enabled, etc.
-	
+
 		// Added in Vite plugin v2.0.0
 		serverOptions: {},
-	
+
 		// Defaults are shown:
 		viteOptions: {
-		// base: githubPath,
-		clearScreen: false,
-		appType: 'mpa', // New in v2.0.0
-		assetsInclude: ['**/*.xml', '**/*.txt', 'CNAME'],
-		base: '/', // use this instead of pathPrefix,
-		publicDir: 'public',
-	
-		// plugins: [pagefind()],
-	
-		server: {
-			mode: 'development',
-			middlewareMode: true
+			// base: githubPath,
+			clearScreen: false,
+			appType: "mpa", // New in v2.0.0
+			assetsInclude: ["**/*.xml", "**/*.txt", "CNAME"],
+			base: "/", // use this instead of pathPrefix,
+			publicDir: "public",
+
+			// plugins: [pagefind()],
+
+			server: {
+				mode: "development",
+				middlewareMode: true,
+			},
+
+			build: {
+				mode: "production",
+			},
+
+			// New in v2.0.0
+			resolve: {
+				alias: {
+					// Allow references to `node_modules` folder directly
+					"/node_modules": path.resolve(".", "node_modules"),
+					"~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+					"~icons": path.resolve(__dirname, "node_modules/bootstrap-icons"),
+				},
+			},
 		},
-	
-		build: {
-			mode: 'production'
-		},
-	
-		// New in v2.0.0
-		resolve: {
-			alias: {
-			// Allow references to `node_modules` folder directly
-			'/node_modules': path.resolve('.', 'node_modules'),
-			'~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-			'~icons': path.resolve(__dirname, 'node_modules/bootstrap-icons')
-			}
-		}
-		}
-	})
+	});
 
 	// Filters
 	eleventyConfig.addFilter("readableDate", (dateObj, format, zone) => {
 		// Formatting tokens for Luxon: https://moment.github.io/luxon/#/formatting?id=table-of-tokens
-		return DateTime.fromJSDate(dateObj, { zone: zone || "utc" }).toFormat(format || "LLLL dd, yyyy");
+		return DateTime.fromJSDate(dateObj, { zone: zone || "utc" }).toFormat(
+			format || "LLLL dd, yyyy"
+		);
 	});
 
-	eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+	eleventyConfig.addFilter("htmlDateString", (dateObj) => {
 		// dateObj input: https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-		return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+		return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
 	});
 
 	// options to open external links in new tab
@@ -88,13 +89,12 @@ module.exports = function(eleventyConfig) {
 		pattern: /^https?:/,
 		attrs: {
 			target: "_blank",
-			rel: "noopener noreferrer"
-		}
+			rel: "noopener noreferrer",
+		},
 	};
 
 	// Customize Markdown library settings:
-	eleventyConfig.amendLibrary("md", mdLib => {
-
+	eleventyConfig.amendLibrary("md", (mdLib) => {
 		mdLib.use(markdownItAttrs, milaOptions);
 
 		mdLib.use(markdownItAnchor, {
@@ -103,34 +103,29 @@ module.exports = function(eleventyConfig) {
 				class: "visually-hidden",
 				symbol: "#",
 				ariaHidden: false,
-				visuallyHiddenClass: 'visually-hidden',
+				visuallyHiddenClass: "visually-hidden",
 			}),
-			level: [1,2,3,4],
-			slugify: eleventyConfig.getFilter("slugify")
+			level: [1, 2, 3, 4],
+			slugify: eleventyConfig.getFilter("slugify"),
 		});
 	});
 
-return {
-templateFormats: [
-"md",
-"njk",
-"html",
-"liquid"
-],
+	return {
+		templateFormats: ["md", "njk", "html", "liquid"],
 
-// Pre-process *.md files with: (default: `liquid`)
+		// Pre-process *.md files with: (default: `liquid`)
 		markdownTemplateEngine: "njk",
 
 		// Pre-process *.html files with: (default: `liquid`)
 		htmlTemplateEngine: "njk",
 
-// These are all optional:
+		// These are all optional:
 		dir: {
-			input: "src",// default: "."
-			includes: "_includes",// default: "_includes"
-			layouts: "_layouts", 
-			data: "_data",// default: "_data"
-			output: "_site"
+			input: "src", // default: "."
+			includes: "_includes", // default: "_includes"
+			layouts: "_layouts",
+			data: "_data", // default: "_data"
+			output: "_site",
 		},
 
 		// -----------------------------------------------------------------
@@ -144,5 +139,5 @@ templateFormats: [
 		// it will transform any absolute URLs in your HTML to include this
 		// folder name and does **not** affect where things go in the output folder.
 		pathPrefix: "/",
-};
+	};
 };
